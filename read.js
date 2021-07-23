074ea1db1f2b973d73062c27c526507fb164bf10
齐年庆 <siasplus@163.com>
2021-07-23 14:18:33 +0800

    first commit

---
+++ read.js
@@ -0,0 +1,23 @@
+/*
+ * @Description: 读取老版excal
+ * @Author: 惜神
+ * @Date: 2021-07-23 13:40:15
+ */
+const xlsx = require('xlsx');
+const read = () => {
+    const workbook = xlsx.readFile("./hh.xls");
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+    let data = []
+    for (value in worksheet) {
+        if (value[0] === '!') continue;
+        let key = Number(value.slice(1, value.length)) - 1;
+        if (!(data[key] instanceof Array)) {
+            data[key] = [];
+        }
+        data[key].push(worksheet[value].v);
+    }
+    return data;
+}
+const result = read('./hh.xls', 'Sheet1');
+console.log(result) // [ [ '姓名', '城市', '年龄' ], [ '齐年庆', '南阳', 18 ], [ '伟峰', '郑卓', 25 ] ]
